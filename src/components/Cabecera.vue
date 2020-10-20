740bfa0156bfa26eb28cab27089b518c984576db
victorm0612 <rojasvictorvargas@gmail.com>
2020-10-20 14:56:14 -0500

    adding scripts from Bootstrap

--- src/components/Cabecera.vue
+++ src/components/Cabecera.vue
@@ -1,17 +1,17 @@
 <template>
     <div id="nav-bar">
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse form-inline my-2 my-lg-0" id="navbarTogglerDemo01">
-            <router-link class="navbar-brand" active-class="active" to="/">
+        <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+        <router-link class="navbar-brand" active-class="active" to="/">
                 <img class="mr-2" width="40" height="40" src="@/assets/bored-shrek-meme.png" alt="">
                 A.B.M.O.D.E.L.
-            </router-link>
+        </router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <router-link class="nav-link" active-class="active" to="/">Create</router-link>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/">Create</router-link>
             </li>
             <li class="nav-item">
                 <router-link class="nav-link" to="/read">Read</router-link>
@@ -38,6 +38,10 @@
     #nav-bar{
         padding: 30px;
         font-size: 1.2rem;
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
     }
     
 </style>
