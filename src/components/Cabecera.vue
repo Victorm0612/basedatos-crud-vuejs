a5fb3a3b5bee11384eaafbc5b935e10f30402e40
victorm0612 <rojasvictorvargas@gmail.com>
2020-10-19 22:34:43 -0500

    ajustes de navbar

---
+++ src/components/Cabecera.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="nav-bar">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse form-inline my-2 my-lg-0" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">A.B.M.O.D.E.L.</a>
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Create</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Read</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Update</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Delete</a>
+            </li>            
+            </ul>
+        </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Cabecera'
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cabecera/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Cabecera from '@/components/Cabecera.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Cabecera
   }
 }
 </script>
